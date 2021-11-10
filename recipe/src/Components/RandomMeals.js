44684993fdbb45f97a856a06a2c4b072e28d2f3f
Adrian Pantea <adrianpantea10@gmail.com>
2021-11-10 14:35:24 +0000

    fetching data from single random meal from API

--- recipe/src/Components/RandomMeals.js
+++ recipe/src/Components/RandomMeals.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const RandomMeals = () => {
+
+  const [meal, setMeal] = useState(undefined)
+
+  const apiURL = 'https://www.themealdb.com/api/json/v1/1/random.php'
+
+  useEffect(() => {
+    async function retrieveMeal() {
+      const res = await fetch(apiURL)
+      console.log('what is res ->', res)
+      const data = await res.json()
+      console.log('what is data ->', data)
+    }
+    retrieveMeal()
+  }, [])
+
   return (
     <div>
-      random meals component
+      
     </div>
   );
 };
