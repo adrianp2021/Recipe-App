a7daf0ef70189668363d503edee36cf1fba75b59
Adrian Pantea <adrianpantea10@gmail.com>
2021-11-10 14:40:09 +0000

    setting state with API response, i.e., random meal

--- recipe/src/Components/RandomMeals.js
+++ recipe/src/Components/RandomMeals.js
@@ -1,24 +1,26 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 const RandomMeals = () => {
+  const [meal, setMeal] = useState(undefined);
 
-  const [meal, setMeal] = useState(undefined)
-
-  const apiURL = 'https://www.themealdb.com/api/json/v1/1/random.php'
+  const apiURL = "https://www.themealdb.com/api/json/v1/1/random.php";
 
   useEffect(() => {
     async function retrieveMeal() {
-      const res = await fetch(apiURL)
-      console.log('what is res ->', res)
-      const data = await res.json()
-      console.log('what is data ->', data)
+      const res = await fetch(apiURL);
+      console.log("what is res ->", res);
+      const data = await res.json();
+      console.log("what is data ->", data);
+      setMeal(data.meals[0])
     }
-    retrieveMeal()
-  }, [])
+    retrieveMeal();
+  }, []);
+
+  if (!meal) return null;
 
   return (
     <div>
-      
+    <div>{meal.strMeal}</div>
     </div>
   );
 };
